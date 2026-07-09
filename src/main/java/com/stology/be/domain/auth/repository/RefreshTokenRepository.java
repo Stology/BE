07a3d6ffbac1c5661ce7f3e6ab5be911f194de18
thepackour <thepackour5610@gmail.com>
2026-07-10 00:10:59 +0900

    fix: 타입 오류 수정 String -> Optional

--- src/main/java/com/stology/be/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/stology/be/domain/auth/repository/RefreshTokenRepository.java
@@ -1,7 +1,9 @@
 package com.stology.be.domain.auth.repository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository {
-    void save(String email, String refreshToken, long ttlSeconds);
-    String findByMemberId(String email);
-    void delete(String email);
+    void save(String uid, String refreshToken, long ttlSeconds);
+    Optional<String> findBySocialUid(String uid);
+    void delete(String uid);
 }
