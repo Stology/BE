b9ff366fd7483ef7c25b858dbdd367c433cc7001
thepackour <thepackour5610@gmail.com>
2026-07-09 10:25:29 +0900

    fix: AuthResDTO.Login 구조 변경 accessToken만 body로 보내고 refreshToken은 쿠키로 전송 Reviewed-by: coderabbitai

--- src/main/java/com/stology/be/domain/auth/dto/AuthResDTO.java
+++ src/main/java/com/stology/be/domain/auth/dto/AuthResDTO.java
@@ -2,7 +2,6 @@ package com.stology.be.domain.auth.dto;
 
 public class AuthResDTO {
     public record Login(
-            String accessToken,
-            String refreshToken
+            String accessToken
     ) {}
 }
