ab688e35902dad1faf1cd10d092d788cca0ff04f
GiYong Kim <153297310+GiY5ngKim@users.noreply.github.com>
2026-07-26 17:07:42 +0900

    fix : 누락된 import 경로 수정

--- src/test/java/com/stology/be/domain/report/converter/ReportJsonConvertersTest.java
+++ src/test/java/com/stology/be/domain/report/converter/ReportJsonConvertersTest.java
@@ -1,7 +1,7 @@
 package com.stology.be.domain.report.converter;
 
-import com.stology.be.domain.report.dto.MemberActivityStatisticsDto;
-import com.stology.be.domain.report.dto.RecommendedNodeDto;
+import com.stology.be.global.external.ai.dto.MemberActivityStatisticsDto;
+import com.stology.be.global.external.ai.dto.RecommendedNodeDto;
 import com.stology.be.domain.report.dto.WeeklyCoreNodeDto;
 import org.junit.jupiter.api.Test;
 
